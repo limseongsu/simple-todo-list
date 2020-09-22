55779e239c477c89ddfdff1376e197d739cf1c24
yunso <cw2cu96@gmail.com>
2020-09-22 18:32:12 +0900

    [FIX] MainActivity.kt 파일의 불필요한 import 삭제

--- app/src/main/java/com/yxnsx/simpletodolist/MainActivity.kt
+++ app/src/main/java/com/yxnsx/simpletodolist/MainActivity.kt
@@ -3,9 +3,7 @@ package com.yxnsx.simpletodolist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.yxnsx.simpletodolist.databinding.ActivityMainBinding
