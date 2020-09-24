7ba77585da04893a057b7d489198a0c8ca721683
yunso <cw2cu96@gmail.com>
2020-09-24 16:13:41 +0900

    [REFACTOR] TodoAdapter.kt 파일에 주석 추가

--- app/src/main/java/com/yxnsx/simpletodolist/TodoAdapter.kt
+++ app/src/main/java/com/yxnsx/simpletodolist/TodoAdapter.kt
@@ -9,7 +9,7 @@ import com.yxnsx.simpletodolist.databinding.ItemTodoBinding
 
 
 class TodoAdapter(
-    private var documentSnapshotList: List<DocumentSnapshot>,
+    var documentSnapshotList: List<DocumentSnapshot>,
     val onClickDeleteIcon: (todo: DocumentSnapshot) -> Unit,
     val onClickTodoItem: (todo: DocumentSnapshot) -> Unit
 ) :
@@ -23,29 +23,40 @@ class TodoAdapter(
         parent: ViewGroup,
         viewType: Int
     ): TodoViewHolder {
+        // item_todo 뷰 인플레이팅
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_todo, parent, false)
+        // 뷰바인딩 및 뷰홀더 생성
         return TodoViewHolder(ItemTodoBinding.bind(view))
     }
 
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        // documentSnapshotList의 인덱스 값 바탕으로 todo 객체 생성
         val todo = documentSnapshotList[position]
 
+        // todo 객체의 isDone 불리언 값이 true일 경우
         if (todo.getBoolean("isDone") == true) {
+            // textViewTodo에 취소선 적용
             holder.todoBinding.textViewTodo.apply {
                 paintFlags = paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
             }
-        } else {
+
+        } else { // todo 객체의 isDone 불리언 값이 false일 경우
             holder.todoBinding.textViewTodo.apply {
+                //textViewTodo에 취소선 미적용
                 paintFlags = 0
             }
         }
 
         holder.todoBinding.apply {
+            // 각 뷰홀더의 textViewTodo에 todo 객체의 text 값 적용
             textViewTodo.text = todo.getString("text")
+
+            // 삭제 버튼 클릭리스너 설정
             imageButtonDelete.setOnClickListener {
                 onClickDeleteIcon.invoke(todo)
             }
+            // 아이템 클릭리스너 설정
             root.setOnClickListener {
                 onClickTodoItem.invoke(todo)
             }
