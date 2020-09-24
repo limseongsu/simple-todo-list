979ae91380f8eb24f64580ffe43be883dd4a614b
yunso <cw2cu96@gmail.com>
2020-09-24 16:44:52 +0900

    [ADD] TodoModel.kt 파일에 fireStore 서버 타임스탬프 값 추가

--- app/src/main/java/com/yxnsx/simpletodolist/TodoModel.kt
+++ app/src/main/java/com/yxnsx/simpletodolist/TodoModel.kt
@@ -1,6 +1,10 @@
 package com.yxnsx.simpletodolist
 
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.ServerTimestamp
+
 data class TodoModel(
     val text: String,
-    var isDone: Boolean = false
+    var isDone: Boolean = false,
+    @ServerTimestamp val timeStamp: Timestamp? = null
 )
